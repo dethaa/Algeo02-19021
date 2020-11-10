9ecdc5bf4a7fbcadcb5fe96ef574e1e213ed791a
dethaa <sharonmarbun12@gmail.com>
2020-11-10 23:22:17 +0700

    mengganti directory dokumen

--- src/SearchEngine.py
+++ src/SearchEngine.py
@@ -1,18 +1,13 @@
 # FILE MAIN
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
-
-import Input_File as IF
-import Tab_Frekuensi as TF 
+import Tab_Frekuensi as TF
 import Tab_Info as TI
-
 
 print("################################## MY SIMPLE SEARCH ENGINE ##################################")
 print("Hasil Pencarian:")
-for jumlahDoc in range (15):
-	print(jumlahDoc+1)
-	print("Jumlah kata: ", TI.Tab_countKata[jumlahDoc])
-	print("Tingkat Kemiripan: ", TI.Tab_Sim[jumlahDoc])
-	print("<Kalimat pertama dari Dokumen ", TI.Index_SortedSim[jumlahDoc], " >")
-#print Tabel Frekuensi mentah
+for jumlahDoc in range(15):
+    print(jumlahDoc + 1)
+    print("Jumlah kata: ", TI.Tab_countKata[jumlahDoc])
+    print("Tingkat Kemiripan: ", TI.Tab_Sim[jumlahDoc])
+    print("Kalimat pertama dari Dokumen ", TI.Index_SortedSim[jumlahDoc], " >")
+# print Tabel Frekuensi mentah
 print(TF.tab_frek)
