832dffb0db789f666b443766575f301e466de915
dethaa <sharonmarbun12@gmail.com>
2020-11-10 12:17:41 +0700

    menambahkan ukuran array clean dan menambahkan komentar

--- src/Input_File.py
+++ src/Input_File.py
@@ -1,11 +1,14 @@
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 
-# Input file
+#fungsi untuk menghapus stopword dari dokumen
 factory1 = StopWordRemoverFactory()
 stopword = factory1.create_stop_word_remover()
+#fungsi untuk menyederhanakan kata ke bentuk dasar
 factory2 = StemmerFactory()
 stemmer = factory2.create_stemmer()
+
+#menginput file
 f1 = open('d1.txt', 'r')
 f2 = open('d2.txt', 'r')
 f3 = open('d3.txt', 'r')
@@ -16,9 +19,13 @@ f7 = open('d7.txt', 'r')
 f8 = open('d8.txt', 'r')
 f9 = open('d9.txt', 'r')
 f10 = open('d10.txt', 'r')
-d = ['*' for i in range(10)]
-stop = ['*' for i in range(10)]
-clean =['*' for i in range(10)]
+
+#deklarasi array
+d = ['*' for i in range(10)] #menyimpan dokumen asli
+stop = ['*' for i in range(10)] #menyimpan hasil penghapusan stopword
+clean =['*' for i in range(11)] #menyimpan hasil pembersihan dokumen (stopword + stemming)
+
+#menyimpan pembacaan dokumen ke dalam array d
 d[0] = f1.read()
 d[1] = f2.read()
 d[2] = f3.read()
@@ -30,10 +37,12 @@ d[7] = f8.read()
 d[8] = f9.read()
 d[9] = f10.read()
 
+#traversal untuk "pembersihan" dokumen
 for i in range(10):
     stop[i] = stopword.remove(d[i])
-    clean[i] = stemmer.stem(stop[i])
+    clean[i+1] = stemmer.stem(stop[i])
 
+#menutup file
 f1.close()
 f2.close()
 f3.close()
