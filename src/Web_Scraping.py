823b774db49e780124631b9b7c77c7ee6c509965
dethaa <sharonmarbun12@gmail.com>
2020-11-14 14:04:44 +0700

    memperbaiki cara menerima link

--- src/Web_Scraping.py
+++ src/Web_Scraping.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-import Input_File as IF 
+
 ################# WEB SCRAPING ##################
-nLink = 2
-isiDokumen = ['*' for i in range(2)]
-link = ['https://id.wikipedia.org/wiki/Kecerdasan_buatan','https://www.dewaweb.com/blog/kecerdasan-buatan/']
+input = input("Masukkan link: ")
+
+#memasukkan link unik pada input ke dalam array link
+link = list(set(input.split()))
+
+#menyimpan banyaknya jumlah link ke dalam nLink
+nLink = len(link)
 HEADERS = {'User-Agent': 'NubesSkuy'}
+
+#memasukkan teks hasil web-scraping ke dalam array isiDokumen
+isiDokumen = ['*' for i in range(nLink)]
 for i in range(nLink):
 	url = Request(link[i],headers=HEADERS)
 	page = urlopen(url)
@@ -13,9 +20,9 @@ for i in range(nLink):
 	soup = BeautifulSoup(html, "html.parser")
 	dokumen = soup.get_text()
 	isiDokumen[i] = dokumen.encode('utf-8')
-link_list = []
-for i in range(2):
-	link_list.append(i)
+
+#memasukkan teks pada array isiDokumen ke dalam folder test
+for i in range(nLink):
 	fname = "../test/pranala" + str(i) + ".txt"
 	with open(fname,'wb') as text_file:
 		text_file.write(isiDokumen[i])
