cfb789f1db081d85145d3fd4195bc99cca2b7ccd
epata <epatatuah180901@gmail.com>
2020-11-14 23:36:40 +0700

    mengubah metode judul file export dari Web_Scraping.py

--- src/Web_Scraping.py
+++ src/Web_Scraping.py
@@ -12,17 +12,21 @@ def web_scrap(input):
 
 	#memasukkan teks hasil web-scraping ke dalam array isiDokumen
 	isiDokumen = ['*' for i in range(nLink)]
+	judulWeb = ['*' for i in range(nLink)]
+	slash = chr(92)
 	for i in range(nLink):
 		url = Request(link[i], headers=HEADERS)
 		page = urlopen(url)
 		html = page.read().decode("utf-8")
 		soup = BeautifulSoup(html, "html.parser")
+		judulTemp = soup.title.string
+		judulWeb[i] = judulTemp.partition('|')[0] or judulTemp.partition('<')[0] or judulTemp.partition('>')[0] or judulTemp.partition('"')[0] or judulTemp.partition('?')[0] or judulTemp.partition('*')[0] or judulTemp.partition(':')[0] or judulTemp.partition('/')[0] or judulTemp.partition(slash)[0]
 		dokumen = soup.get_text()
 		isiDokumen[i] = dokumen.encode('utf-8')
 
 	#memasukkan teks pada array isiDokumen ke dalam folder test
 	for i in range(nLink):
-		fname = "../test/link " + link[i] + ".txt"
+		fname = "../test/link " + judulWeb[i] + ".txt"
 		with open(fname, 'wb') as text_file:
 			text_file.write(isiDokumen[i])
 
